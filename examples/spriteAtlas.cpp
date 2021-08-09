4d39aef833550249bb8bbec306584283f078e7ec
Danieljb5 <danieljburfoot@outlook.com>
2021-08-09 10:02:39 +0100

    small code cleanup

--- examples/spriteAtlas.cpp
+++ examples/spriteAtlas.cpp
@@ -54,31 +54,6 @@ public:
 
         clear();
 
-        // for(int i = 0; i < sprites.size(); i++)
-        // {
-        //     float x = sprites[i]->getX();
-        //     float y = sprites[i]->getY();
-        //     float vx = vxs.at(i); 
-        //     float vy = vys.at(i);
-        //     if(vx == 0) vx = 100;
-        //     if(vy == 0) vy = 100;
-
-        //     x += (vx * time.dt);
-        //     y += (vy * time.dt);
-
-        //     if(x <= 0 || x + 218 >= screenWidth()) vx *= -1;
-        //     if(y <= 0 || y + 153 >= screenHeight()) vy *= -1;
-        //     if(x < 0) x = 0;
-        //     if(x >= screenWidth()) x = screenWidth() - 1;
-        //     if(y < 0) y = 0;
-        //     if(y >= screenHeight()) y = screenHeight() - 1;
-
-        //     sprites[i]->setX(x);
-        //     sprites[i]->setY(y);
-        //     vxs[i] = vx;
-        //     vys[i] = vy;
-        // }
-
         appName = "Example Sprite Atlas | Sprites: " + std::to_string(numSprites);
     }
 
